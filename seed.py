cab2ef594dd6eb32fd1bbdba7629c1e1df1f7da7
Hackbright Student <student@hackbrightacademy.com>
2015-10-20 13:05:09 -0700

    seed.py has all functions, need to debug

--- seed.py
+++ seed.py
@@ -2,11 +2,12 @@
 
 
 from model import User
-# from model import Rating
-# from model import Movie
+from model import Rating
+from model import Movie
 
 from model import connect_to_db, db
 from server import app
+from datetime import datetime
 
 
 def load_users():
@@ -36,10 +37,75 @@ def load_users():
 
 def load_movies():
     """Load movies from u.item into database."""
+    print "Movies"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    Movie.query.delete()
+
+    # Read u.item file and insert data
+    for row in open("seed_data/u.item"):
+        row =row.rstrip()
+
+        movie_id, title_long, released_string, imdb_url = row.split("|")[:4]
+        #we modified the datetime format changed released_string into 
+        #new format by using datetim.strptime to convert it. 
+        print row
+        if released_string: 
+            release_at = datetime.strptime(released_string, "%d-%b-%Y")
+        else: 
+            release_at = None 
+
+        #here we stripped the title of the (xxxx) year and parenthesis
+        #using the slice method. 
+        title = title_long[:-7]
+
+        print movie_id, title_long, released_string, imdb_url
+
+        #assign the return values from our for loop to a new variable
+        movie = Movie(movie_id=movie_id, title=title, released_at=release_at,
+                      imdb_url=imdb_url)
+    
+        # We need to add to the session or it won't ever be stored
+        db.session.add(movie)
+
+    #Once we're done, we should commit our work
+    db.session.commit()
 
 
 def load_ratings():
     """Load ratings from u.data into database."""
+
+    print "Ratings"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    Rating.query.delete()
+
+    # Read u.data file and insert data
+    for row in open("seed_data/u.data"):
+        row = row.rstrip()
+        rating_id, movie_id, user_id, score = row.split("\t")
+
+        user_id = int(user_id)
+        movie_id = int(movie_id)
+        score = int(score)
+
+        rating = Rating(rating_id=rating_id, 
+                        movie_id=movie_id, user_id=user_id, score=score)
+
+        #We need to add to the session or it won't ever be stored
+        db.session.add(rating)
+
+    #Once we're done, we should commit our work
+    db.session.commit()
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
